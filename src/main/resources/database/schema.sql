2e0af68bede16afa58055f3ea2505528272029e2
ErikaGenova <erika.genova@stud.unifi.it>
2023-08-18 18:54:07 +0200

    Schema database

---
+++ src/main/resources/database/schema.sql
@@ -0,0 +1,56 @@
+-- schema per le tabelle del database
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS advertisers;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS favorites;
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    address TEXT NOT NULL,
+    city TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    sqrmt INTEGER NOT NULL,
+    sell BOOLEAN NOT NULL,
+    advertiser_id TEXT NOT NULL,
+    FOREIGN KEY (advertiser_id) REFERENCES advertisers(id)
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id INTEGER PRIMARY KEY,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    id_ad INTEGER NOT NULL,
+    id_client TEXT NOT NULL,
+    FOREIGN KEY (id_ad) REFERENCES ads(id)
+    FOREIGN KEY (id_client) REFERENCES clients(fiscal_code)
+);
+
+
+CREATE TABLE IF NOT EXISTS clients (
+    fiscal_code TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    budget INTEGER NOT NULL,
+);
+
+
+CREATE TABLE IF NOT EXISTS advertisers (
+    id TEXT PRIMARY KEY,
+    bank_account INTEGER NOT NULL,
+    agency_name TEXT,
+    agency_fee INTEGER,
+    private_owners_name TEXT,
+    private_owners_last_name TEXT,
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+    id_ad INTEGER NOT NULL,
+    id_client TEXT NOT NULL,
+    PRIMARY KEY (id_ad, id_client),
+    FOREIGN KEY (id_ad) REFERENCES ads(id)
+    FOREIGN KEY (id_client) REFERENCES clients(fiscal_code)
+);
